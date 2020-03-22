dda4e9cf5373ae7c138656dee8f3c56490151f4d
Taulant Ibraj <tibraj94@gmail.com>
2020-03-22 17:06:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
   board.each do |turn|
     if turn = "X" || turn = "O"
-      return "board#{turn}".to_i
+      return "board[#{turn}]".to_i
       counter += 1
     end
   end
